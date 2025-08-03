4a0e66267f0b81a523db3dd7ab565583be6fd3a3
Nguyen Duc Thang <nguyenducthang899@gmail.com>
2025-08-04 06:38:11 +0700

    Update 1

---
+++ vite-project/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+    return (
+       <div class ="min-h-screen"> 
+        {/* Theme Toggle*/}
+
+        {/* Background Effects */}
+
+        {/* Navbar */}
+        
+        {/* Main Content */}
+
+        {/* Footer */}
+       </div>
+    );
+};
