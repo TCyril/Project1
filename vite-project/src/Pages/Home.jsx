cc9bed00cebfc0ed6874db85ae1a47a25e8577d5
Nguyen Duc Thang <nguyenducthang899@gmail.com>
2025-08-05 02:50:57 +0700

    add starBackground

--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import { ThemeToggle } from "../Components/ThemeToggle";
+import { StarBackground } from "@/Components/StarBackground";
 
 export const Home = () => {
     return (
@@ -6,7 +7,7 @@ export const Home = () => {
         {/* Theme Toggle*/}
         <ThemeToggle />
         {/* Background Effects */}
-
+        <StarBackground />
         {/* Navbar */}
         
         {/* Main Content */}
