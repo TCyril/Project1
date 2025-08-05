ec4b270edac61790cbd7f4fce2c6724c7c8710a4
Nguyen Duc Thang <nguyenducthang899@gmail.com>
2025-08-05 18:43:33 +0700

    Update starBackground Add navBar

--- vite-project/src/Components/StarBackground.jsx
+++ vite-project/src/Components/StarBackground.jsx
@@ -1,12 +1,25 @@
 import { useEffect, useState } from "react";
 
-// id, size, x, y, opacity, animationDuration
+// id, size, x, y, opacity, animationDuration => for stars
+// id, size, x, y, delay, animationDuration => for meteors
 
 export const StarBackground = () => {
     const [stars, setStars] = useState([]);
+    const [meteors, setMeteors] = useState([]);
 
     useEffect(() => {
         generateStars();
+        generateMeteors();
+
+        const handleResize = () => {
+            generateStars();
+        };
+         
+        window.addEventListener('resize', handleResize);
+        
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, []);
 
     const generateStars = () => {
@@ -18,27 +31,65 @@ export const StarBackground = () => {
         for (let i = 0; i < numberOfStars; i++) {
             newStars.push({
                 id:i,
-                size: Math.random() * 2 + 1,
+                size: Math.random() * 3 + 1,
                 x: Math.random() * 100,
                 y: Math.random() * 100,
                 opacity: Math.random() * 0.5 + 0.5,
-                animationDuration: Math.random() * 4 * 2,
+                animationDuration: Math.random() * 4 + 2,
             });
         }
 
         setStars(newStars);
     };
+
+    const generateMeteors = () => {
+        const numberOfMeteors = 7;
+
+        const newMeteors = [];
+
+        for (let i = 0; i < numberOfMeteors; i++) {
+            newMeteors.push({
+                id:i,
+                size: Math.random() * 2 + 1,
+                x: Math.random() * 100,
+                y: Math.random() * 20,
+                delay: Math.random() * 15,
+                animationDuration: Math.random() * 4 + 2,
+            });
+        }
+
+        setMeteors(newMeteors);
+    };
+
+
     return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
         {stars.map((star) => (
-            <div key={star.id} className="star animate-pulse-subtle" style={{
-                width: star.size + 'px',
-                height: star.size + 'px',
-                left: star.x + '%',
-                top: star.y + '%',
-                opacity: star.opacity,
-                animationDuration: star.animationDuration + 's',
-            }}/>
+            <div key={star.id} 
+                className="star animate-pulse-subtle" 
+                style={{
+                    width: star.size  + 'px',
+                    height: star.size  + 'px',
+                    left: star.x + '%',
+                    top: star.y + '%',
+                    opacity: star.opacity,
+                    animationDuration: star.animationDuration + 's',
+                }}
+            />
+        ))}
+
+        {meteors.map((meteor) => (
+            <div key={meteor.id} 
+                className="meteor animate-meteor" 
+                style={{
+                    width: meteor.size * 50 + 'px',
+                    height: meteor.size + 'px',
+                    left: meteor.x + '%',
+                    top: meteor.y + '%',
+                    animationDelay: meteor.delay,
+                    animationDuration: meteor.animationDuration + 's',
+                }}
+            />
         ))}
     </div>
     );
--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,16 +1,20 @@
 import { ThemeToggle } from "../Components/ThemeToggle";
 import { StarBackground } from "@/Components/StarBackground";
+import { Navbar } from "@/Components/Navbar";  
 
 export const Home = () => {
     return (
        <div className ="min-h-screen bg-background text-foreground overflow-x-hidden"> 
         {/* Theme Toggle*/}
         <ThemeToggle />
+
         {/* Background Effects */}
         <StarBackground />
+
         {/* Navbar */}
-        
+        <Navbar />
         {/* Main Content */}
+    
 
         {/* Footer */}
        </div>
